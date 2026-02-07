22b6ea3bf39ca6f67d93fd91760ca2e2400af381
tranvietanh0 <tranvietanh0507@gmail.com>
2026-02-07 10:43:21 +0700

    Add new meta files and implement SceneToolBarExtend for enhanced scene management

---
+++ Editor/Misc/NonDrawingGraphicEditor.cs
@@ -0,0 +1,25 @@
+﻿/// Credit Slipp Douglas Thompson
+/// Sourced from - https://gist.github.com/capnslipp/349c18283f2fea316369
+///
+
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace UnityEngine.UI.Extensions
+{
+    using GameFoundationCore.Scripts.UIModule.Utilities.UIStuff;
+
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(NonDrawingGraphic), false)]
+    public class NonDrawingGraphicEditor : GraphicEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            this.serializedObject.Update();
+            EditorGUILayout.PropertyField(this.m_Script, new GUILayoutOption[0]);
+            // skipping AppearanceControlsGUI
+            this.RaycastControlsGUI();
+            this.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
